cbcb8ace0fcec5321fba2f06d85e5a0f6fc60032
holgerm <mail@holgermuegge.de>
2018-05-30 15:27:46 +0200

    spawn automatisch T-Shirt. Zufällig gedreht.

---
+++ T-Shirt Minigame/Assets/Scripts/Spawn.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour
+{
+	 public Rigidbody2D itemToSpawn;
+
+	 // Use this for initialization
+	 void Start ()
+	 {
+	 }
+
+	 void spawn ()
+	 {
+		  
+		  Instantiate(itemToSpawn, transform.position, Quaternion.AngleAxis(Random.Range(0.0f, 360.0f), Vector3.forward));
+	 }
+
+	 private float nextActionTime = 0.0f;
+	 public float secondsBetweenSpawn = 3f;
+
+	 void Update ()
+	 {
+		  if ( Time.time > nextActionTime ) {
+				nextActionTime += secondsBetweenSpawn;
+
+				spawn();
+		  }
+	 }
+
+}
