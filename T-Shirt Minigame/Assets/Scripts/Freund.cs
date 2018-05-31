b9330109d4ae877d4775d1f257cd5eaebafe248b
holgerm <mail@holgermuegge.de>
2018-05-31 08:11:24 +0200

    Freund / Container nimmt TShirts nur noch oben auf

---
+++ T-Shirt Minigame/Assets/Scripts/Freund.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Freund : MonoBehaviour
+{
+
+	 public TextMesh counterText;
+
+	 private int counter = 0;
+
+	 void OnTriggerEnter2D (Collider2D other)
+	 {
+		  Debug.Log("FREUND freut sich!");
+		  Destroy(other.gameObject);
+
+		  counter++;
+		  counterText.text = "" + counter;
+	 }
+
+
+}
