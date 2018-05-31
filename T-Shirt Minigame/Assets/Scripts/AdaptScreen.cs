0385036d25ee7d28d62d1f3a960daccda0b9bfbb
holgerm <mail@holgermuegge.de>
2018-05-31 20:01:44 +0200

    screensize adapted

---
+++ T-Shirt Minigame/Assets/Scripts/AdaptScreen.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AdaptScreen : MonoBehaviour
+{
+
+	 // Use this for initialization
+	 void Start ()
+	 {
+		  Screen.SetResolution(1440, 2560, true);	
+	 }
+	
+	 // Update is called once per frame
+	 void Update ()
+	 {
+		
+	 }
+}
