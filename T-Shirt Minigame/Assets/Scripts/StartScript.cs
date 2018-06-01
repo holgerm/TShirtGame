5738d67de42ee9ab9b1a0017f9100916760515c6
dietsch@qdrei.info <dietsch@qdrei.info>
2018-06-01 13:16:06 +0200

    start szene

---
+++ T-Shirt Minigame/Assets/Scripts/StartScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	public void Startgame () {
+
+        UnityEngine.SceneManagement.SceneManager.LoadScene("MainScene");
+
+
+
+    }
+}
