b508ffbd751da6157f29d263a05020b98db95787
dietsch@qdrei.info <dietsch@qdrei.info>
2018-06-01 13:16:17 +0200

    aufhalter unten

---
+++ T-Shirt Minigame/Assets/Scripts/Container2.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Container2 : MonoBehaviour
+{
+
+void OnTriggerEnter2D(Collider2D other)
+    {
+
+        if (other.tag == "AltesTShirt")
+        {
+           
+            Destroy(other.gameObject);
+        }
+
+        if (other.tag == "NormalesTShirt")
+        {
+                        Destroy(other.gameObject);
+        }
+
+
+
+
+    }
+
+
+}
