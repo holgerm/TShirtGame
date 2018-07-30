5df644fe60b4a9d69ecd00c3de3ea582634bd25e
dietsch@qdrei.info <dietsch@qdrei.info>
2018-07-30 16:27:22 +0200

    t-shirt kann man wieder anfassen

--- T-Shirt Minigame/Assets/Scripts/Swipable.cs
+++ T-Shirt Minigame/Assets/Scripts/Swipable.cs
@@ -4,11 +4,10 @@ using UnityEngine;
 
 public class Swipable : MonoBehaviour
 {
-    public int AnzahlTshirtsanZeiger;
+
     // Use this for initialization
     void Start ()
 	 {
-        AnzahlTshirtsanZeiger = 0;
     }
     
     void Update ()
@@ -21,16 +20,15 @@ public class Swipable : MonoBehaviour
 		  bool overSprite = this.GetComponent<SpriteRenderer>().bounds.Contains(mousePosition);
 
 		  //If it's over the sprite
-		  if ( overSprite && AnzahlTshirtsanZeiger > 2) {
+		  if ( overSprite) {
 				//If we've pressed down on the mouse (or touched on the iphone)
 				if ( Input.GetButton("Fire1") ) {
 					 //Set the position to the mouse position
 					 this.transform.position = new Vector3(Camera.main.ScreenToWorldPoint(Input.mousePosition).x,
 						  Camera.main.ScreenToWorldPoint(Input.mousePosition).y,
 						  0.0f);
-                AnzahlTshirtsanZeiger++;
+
             }
-            else { AnzahlTshirtsanZeiger--; }
 		  }
 	 }
 }
