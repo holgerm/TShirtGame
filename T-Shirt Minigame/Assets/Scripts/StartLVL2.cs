ff55aaf59125f113d75d8686b57777cdf994932f
dietsch@qdrei.info <dietsch@qdrei.info>
2018-07-30 15:41:24 +0200

    Alles fertig auch Level 2 lolllllllölll

---
+++ T-Shirt Minigame/Assets/Scripts/StartLVL2.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartLVL2 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	public void Click () { 	
+	UnityEngine.SceneManagement.SceneManager.LoadScene("Level 2");
+}
+}
+
+
